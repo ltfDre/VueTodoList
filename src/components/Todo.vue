afafad0892280cf6dd32e91d2d95452f6a847f68
ltfDre <ltfDre@gmail.com>
2019-09-05 17:38:48 +0300

    TodoList

---
+++ src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row">
+        <div class="col-md-12 border off-set-md-3 d-flex flex-row justify-content-between align-items-center p-2 pb-2">
+          <span class="font-weight-lighter">{{ todo.text }}</span>
+          <button class="btn btn-danger" @click="$emit('deleteTodo', todo.id)">Delete</button>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    props: ["todo"]
+}
+</script>
